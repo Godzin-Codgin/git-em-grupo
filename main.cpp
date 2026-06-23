fc19b874af7707f735e870530dcf9dd00999445a
THIAGO SILVA SANTOS <thiago_santos172@estudante.sesisenai.org.br>
2026-06-23 15:13:51 -0300

    adicionado saudacao

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void saudacao() {
+	cout << "bem vindo ao sistema!" << endl;
+}
+
 int main(){
-	
+	saudacao();
 	cout<<"projeto em grupo"<<endl;
 	
 	return 0;
