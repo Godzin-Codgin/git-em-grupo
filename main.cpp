a507b9ec7b92884eba6dd0ef0ff25f9b559628a7
MARIA EDUARDA SILVA MARMENTINI <maria_e_marmentini@estudante.sesisenai.org.br>
2026-06-23 15:54:06 -0300

    adicionado despedida

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ void saudacao() {
 	cout << "bem vindo ao sistema!" << endl;
 }
 
+void despedida() {
+	cout<< "Obrigado por visualizar o sistema" << endl;
+}
 int main(){
 	saudacao();
 	cout<<"projeto em grupo"<<endl;
-	
+	despedida();
 	return 0;
 }
